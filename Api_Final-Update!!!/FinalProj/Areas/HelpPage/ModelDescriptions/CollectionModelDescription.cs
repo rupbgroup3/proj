b51cd19282bdc768461ff5eace887ac7fef4d9bb
rupbgroup3 <60218857+rupbgroup3@users.noreply.github.com>
2020-04-21 02:51:57 +0300

    Last Update
    Last Update

--- Api_Final-Update!!!/FinalProj/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace FinalProj.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
