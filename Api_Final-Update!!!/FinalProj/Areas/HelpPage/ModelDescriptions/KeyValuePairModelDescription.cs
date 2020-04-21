007d60d5bb2286165f11f5bcc5411c2ec034c5a4
rupbgroup3 <60218857+rupbgroup3@users.noreply.github.com>
2020-04-21 16:32:29 +0300

    update
    update

--- Api_Final-Update!!!/FinalProj/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace FinalProj.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
